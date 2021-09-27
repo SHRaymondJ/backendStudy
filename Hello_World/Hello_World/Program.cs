e539fa82e06edc4ece85e0e78b8cdf97c41f16e6
Raymond <396945399@qq.com>
2021-09-28 07:24:51 +0800

    C#学习，hello world

---
+++ Hello_World/Hello_World/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("欢迎使用移动充值业务，请输入充值金额:");
+            string x = Console.ReadLine();
+            Console.WriteLine("充值成功，您的充值金额为：" + x + "元");
+        }
+    }
+}
